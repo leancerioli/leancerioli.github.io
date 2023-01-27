42d1669fb8b1a053a697cd010147571390111929
leancerioli <123603245+leancerioli@users.noreply.github.com>
2023-01-26 23:44:03 -0300

    Add files via upload
    Navbar mobile responsive

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+/*Conocimientos interactivos*/
 function chbg(id, color) {
   document.getElementById(id).style.backgroundColor = color;
 }
+
+/*Navbar responsive*/
+const listButton = document.querySelector('.mobile')
+const listItems = document.querySelector('#list')
+
+listButton.addEventListener("click", () => {
+    listItems.classList.toggle("hidden")
+})
+
+listItems.addEventListener("click", () => {
+    setTimeout(() => {
+        listItems.classList.add("hidden")
+    }, 150)
+})
